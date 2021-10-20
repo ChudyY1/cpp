f0d738db08b7ad7f782f92dfa09225a8c6d963b1
Paweł Pepliński <92047270+ChudyY1@users.noreply.github.com>
2021-10-20 17:37:41 +0200

    Add files via upload

--- wyklad2/s03-echo.cpp
+++ wyklad2/s03-echo.cpp
@@ -6,10 +6,22 @@ int main(int argc, char* argv[])
     std::string napis;
     napis="";
     napis+=argv[1];
-    if(napis=="-n -r" || "-r -n")
+    if(napis=="-n -r" || napis=="-r -n")
     {
         for(int i=argc-1;i>1;i--){
             std::cout<<argv[i]<<" ";
+        }
+    }
+    else if(napis=="-n -l" || napis=="-l -n")
+    {
+        for(int i=2;i<argc;i++){
+            std::cout<<"\n"<<argv[i];
+        }
+    }
+     else if(napis=="-r -l" || napis=="-l -r")
+    {
+        for(int i=argc-1;i>1;i--){
+            std::cout<<argv[i]<<"\n";
         }
     }
     else if(napis=="-n")
