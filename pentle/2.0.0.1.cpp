24b2f3e02f1f1350cbd96bb3f093d2020d8ca807
Paweł Pepliński <92047270+s24803@users.noreply.github.com>
2021-10-29 13:58:11 +0200

    Add files via upload

--- pentle/2.0.0.1.cpp
+++ pentle/2.0.0.1.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 
-auto przedzial(int a, int b, int c) -> void;
+auto przedzial(int a, int b, int c, int s) -> void;
 auto get() -> int;
 
 auto main() -> int
 {
     auto a = get();
     auto b = get();
+
+    if(a > b)
+    {
+        std::cout << "a nie moze byc wieksze od b\n";
+        a = get();
+        b = get();   
+    }
+
     auto c = get();
-    przedzial(a, b, c);
+    auto s = get();
+    przedzial(a, b, c, s);
        
     return 0;
 }
 
-auto przedzial(int a, int b, int c) -> void
+auto przedzial(int a, int b, int c, int s) -> void
 {
    
     while(c == 0)
@@ -21,10 +30,26 @@ auto przedzial(int a, int b, int c) -> void
         std::cout << "c nie moze byc rowne 0\n";
         c = get();
     }
-   
-    for(auto i=a; i<b; i++)
+    
+    while(s == 0)
     {
-        if(i%c == 0)    std::cout << i << " ";
+        std::cout << "s nie moze byc rowne 0\n";
+        s = get();
+    }
+
+    if(s > 0)
+    {
+        for(auto i=a; i<b; i += s) // i = i + s
+        {
+            if(i%c == 0)    std::cout << i << " ";
+        }
+    }
+    else
+    {
+        for(auto i=b; i>a; i+=s)
+        {
+            if((i-1)%c == 0)    std::cout << i-1 << " ";
+        }
     }
     std::cout << "\n";
 }
