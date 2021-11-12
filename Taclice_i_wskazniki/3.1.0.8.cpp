09730fb9ff901d074af63f8a11d215241e9fedef
Paweł Pepliński <92047270+s24803@users.noreply.github.com>
2021-11-12 14:22:00 +0100

    Add files via upload

---
+++ Taclice_i_wskazniki/3.1.0.8.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <array>
+
+auto sort_asc(int a[], int n) -> void
+{
+	int temp;
+	for(int i = 0; i<n-1 ; i++)
+	{
+		for(int j=i+1; j<n; j++)
+		{
+			if(a[i] < a[j])
+			{
+				temp = a[j];
+				a[j] = a[i];
+				a[i] = temp;
+			}
+		}
+	}
+}
+
+auto main() -> int
+{
+	std::array <int, 10> tab {3,4,6,7,8,9,10,-4,3,1};
+	sort_asc(tab.data(), tab.size());
+	
+	for(auto i=0; i<10; i++)
+	{
+		std::cout << tab[i] << " ";
+	}
+	
+	std::cout << "\n";
+	
+	return 0;
+}
