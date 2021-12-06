0d376cea86b566e3fbc4ce658df0db7ea31a27e5
Paweł Pepliński <92047270+s24803@users.noreply.github.com>
2021-12-06 19:23:42 +0100

    Add files via upload

--- Taclice_i_wskazniki/3.3.0.4.cpp
+++ Taclice_i_wskazniki/3.3.0.4.cpp
@@ -13,31 +13,26 @@ auto print_int(void* a, size_t s) -> void
 
 auto print_str(void* a, size_t s) -> void
 {
-	auto tab = reinterpret_cast<char*>(a);
+	auto tab = reinterpret_cast<std::string*>(a);
 	
 	for(auto i = size_t{0}; i < s; ++i)
 	{
-		std::cout << tab[i];
+		std::cout << tab[i] << std::endl;
 	} 
 	std::cout << std::endl;
 }
 
-auto fpprint(void* a, size_t size_tab, size_t element, void (*fp)(void*, size_t)) -> void
+auto fpprint(void* a, size_t size_tab, void (*fp)(void*, size_t)) -> void
 {
-	if(element == 4)
-		(*fp)(a,size_tab);
-	else
-	{
-		fp = &print_str;
-		(*fp)(a,size_tab);
-	}
+	(*fp)(a,size_tab);
 }
 
 auto main() -> int
 {
 	int tab[3] = {4,1,0};
-	char str[] = "placek";
-	fpprint(tab, sizeof(tab)/sizeof(tab[0]), sizeof(tab[0]), &print_int);
-	fpprint(str, sizeof(str)/sizeof(str[0]), sizeof(str[0]), &print_int);
+	std::string str[3] = {"jeden", "dwa", "trzy"};
+	fpprint(tab, sizeof(tab)/sizeof(tab[0]), &print_int);
+	fpprint(str, sizeof(str)/sizeof(str[0]), &print_str);
+	
 	return 0;
 }
